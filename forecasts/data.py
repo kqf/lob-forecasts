5a3afe3ea1f9bd330121cabff648425d184026fd
kqf <q.dev.null@gmail.com>
2023-06-17 21:09:16 +0200

    Add dataset processing (#2)
    * Add the helper functions
    
    * Add the data loading
    
    * Add the dataset generation

--- forecasts/data.py
+++ forecasts/data.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import numpy as np
+import torch
 
 
 def build_data(
@@ -17,3 +18,49 @@ def build_data(
 
     print(dec_train.shape, dec_val.shape, dec_test.shape)
     return dec_train, dec_val, dec_test
+
+
+def prepare_x(data):
+    df1 = data[:40, :].T
+    return np.array(df1)
+
+
+def prepare_label(data):
+    return data[-5:, :].T
+
+
+def data_classification(X, Y, T):
+    [N, D] = X.shape
+    df = np.array(X)
+    dY = np.array(Y)
+    dataY = dY[T - 1 : N]
+    dataX = np.zeros((N - T + 1, T, D))
+    for i in range(T, N + 1):
+        dataX[i - T] = df[i - T : i, :]
+    return dataX, dataY
+
+
+class LobDataset(torch.utils.data.Dataset):
+    def __init__(self, data, k, num_classes, T):
+        """Initialization"""
+        self.k = k
+        self.num_classes = num_classes
+        self.T = T
+
+        x = prepare_x(data)
+        y = prepare_label(data)
+        x, y = data_classification(x, y, self.T)
+        y = y[:, self.k] - 1
+        self.length = len(x)
+
+        x = torch.from_numpy(x)
+        self.x = torch.unsqueeze(x, 1)
+        self.y = torch.from_numpy(y)
+
+    def __len__(self):
+        """Denotes the total number of samples"""
+        return self.length
+
+    def __getitem__(self, index):
+        """Generates samples of data"""
+        return self.x[index], self.y[index]
