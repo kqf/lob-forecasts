39609f2da97ee8b86eb8e0ea096c59fae162fa14
Sasha <q.dev.null@gmail.com>
2023-06-17 20:24:12 +0200

    Add the fake data

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import pathlib
+
+import numpy as np
+import pytest
+
+
+def fake(path):
+    np.savetxt(path, np.random.rand(149, 10000))
+
+
+@pytest.fixture
+def fake_dataset(tmp_path: pathlib.Path) -> pathlib.Path:
+    datadir = tmp_path / " data"
+    datadir.mkdir(exist_ok=True)
+    fake(datadir / "Train_Dst_NoAuction_DecPre_CF_7.txt")
+    fake(datadir / "Test_Dst_NoAuction_DecPre_CF_7.txt")
+    fake(datadir / "Test_Dst_NoAuction_DecPre_CF_8.txt")
+    fake(datadir / "Test_Dst_NoAuction_DecPre_CF_9.txt")
+    return datadir
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,5 @@
 from forecasts.data import build_data
 
 
-def test_data():
-    build_data()
+def test_data(fake_dataset):
+    build_data(fake_dataset)
