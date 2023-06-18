ca2371fb549b70cf52958bf371a040ad8f2f4f1f
Sasha <q.dev.null@gmail.com>
2023-06-18 07:39:46 +0000

    Make things work

--- forecasts/main.py
+++ forecasts/main.py
@@ -1,34 +1,18 @@
+import skorch
 import torch
-from skorch import NeuralNetClassifier
 
 from forecasts.data import LobDataset, build_data
-from forecasts.model import DeepLob, batch_gd, evaluate
+from forecasts.model import DeepLob
 
 
 def main():
-    batch_size = 64
+    batch_size = 64 * 6
     train, valid, test_ = build_data()
-    train = train[:, :640]
-    valid = valid[:, :640]
-    test_ = test_[:, :640]
-
     dataset_train = LobDataset(data=train, k=4, num_classes=3, T=100)
     dataset_valid = LobDataset(data=valid, k=4, num_classes=3, T=100)
-    dataset_test_ = LobDataset(data=test_, k=4, num_classes=3, T=100)
-
+    # dataset_test_ = LobDataset(data=test_, k=4, num_classes=3, T=100)
     model = DeepLob(y_len=dataset_train.num_classes)
-    # batch_gd(
-    #     model,
-    #     torch.nn.CrossEntropyLoss(),
-    #     torch.optim.Adam(model.parameters(), lr=0.0001),
-    #     train_loader,
-    #     valid_loader,
-    #     epochs=50,
-    #     device=device,
-    # )
-    # model = torch.load("best_val_model_pytorch")
-    # evaluate(model, test__loader, device)
-    model = NeuralNetClassifier(
+    model = skorch.NeuralNetClassifier(
         module=DeepLob,
         module__y_len=dataset_train.num_classes,
         train_split=lambda X: (X, dataset_valid),
@@ -37,6 +21,11 @@ def main():
         optimizer__lr=0.0001,
         batch_size=batch_size,
         iterator_train__shuffle=True,
+        max_epochs=50,
+        device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+        callbacks=[
+            skorch.callbacks.ProgressBar(),
+        ],
     )
     model.fit(dataset_train, None)
 
