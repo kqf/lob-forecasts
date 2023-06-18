14bd89184ba09547d815d797a81b6c7c914b38b9
Sasha <q.dev.null@gmail.com>
2023-06-18 12:33:34 +0200

    Add the learning curve plotting

--- forecasts/main.py
+++ forecasts/main.py
@@ -1,30 +1,47 @@
+import numpy as np
 import skorch
 import torch
+from matplotlib import pyplot as plt
 
 from forecasts.data import LobDataset, build_data
 from forecasts.model import DeepLob
 
 
+class PlotLossCallback(skorch.callbacks.Callback):
+    def on_train_end(self, net, **kwargs):
+        train_loss = net.history[:, "train_loss"]
+        valid_loss = net.history[:, "valid_loss"]
+
+        plt.figure(figsize=(15, 6))
+        plt.plot(np.arange(len(train_loss)), train_loss, label="Train Loss")
+        plt.plot(np.arange(len(valid_loss)), valid_loss, label="Valid Loss")
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
+        plt.legend()
+        plt.show()
+
+
 def main():
-    batch_size = 64 * 6
+    batch_size = 64
     train, valid, test_ = build_data()
-    dataset_train = LobDataset(data=train)
-    dataset_valid = LobDataset(data=valid)
+    dataset_train = LobDataset(data=train[:, :200])
+    dataset_valid = LobDataset(data=valid[:, :200])
+    print(len(dataset_train))
     # dataset_test_ = LobDataset(data=test_)
-    model = DeepLob(y_len=dataset_train.num_classes)
     model = skorch.NeuralNetClassifier(
         module=DeepLob,
-        module__y_len=dataset_train.num_classes,
+        module__num_classes=3,
         train_split=lambda X: (X, dataset_valid),
         criterion=torch.nn.CrossEntropyLoss,
         optimizer=torch.optim.Adam,
         optimizer__lr=0.0001,
         batch_size=batch_size,
         iterator_train__shuffle=True,
-        max_epochs=50,
+        max_epochs=20,
         device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
         callbacks=[
             skorch.callbacks.ProgressBar(),
+            PlotLossCallback(),
         ],
     )
     model.fit(dataset_train, None)
