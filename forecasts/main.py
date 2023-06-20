c416a885920863211493c5f36511570f3a3df2b7
Sasha <q.dev.null@gmail.com>
2023-06-20 13:51:42 +0000

    Add a simplistic caching mechanism

--- forecasts/main.py
+++ forecasts/main.py
@@ -6,6 +6,13 @@ from forecasts.data import files, read_single, to_classification
 from forecasts.timer import timer
 
 
+def no_downsample(
+    X: np.ndarray,
+    y: np.ndarray,
+) -> tuple[np.ndarray, np.ndarray]:
+    return X, y
+
+
 def downsample(X: np.ndarray, y: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     if len(X) == 0:
         return X, y
@@ -19,7 +26,7 @@ def downsample(X: np.ndarray, y: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     return X_res.reshape(-1, *shape[1:]), y_res
 
 
-def build_valid(
+def build_dataset(
     subset,
     scaler,
     downsample=downsample,
@@ -32,7 +39,11 @@ def build_valid(
         xx.append(X_res)
         yy.append(y_res)
 
-    return np.concatenate(xx), np.concatenate(yy)
+    X, y = np.concatenate(xx), np.concatenate(yy)
+    np.save(f"data/X_{subset}.npy", X)
+    np.save(f"data/y_{subset}.npy", y)
+
+    return np.load(f"data/X_{subset}.npy"), np.load(f"data/y_{subset}.npy")
 
 
 def main():
@@ -41,31 +52,23 @@ def main():
         for file in files(subset="train"):
             features, *_ = read_single(file)
             scaler.partial_fit(features)
-            break
 
     with timer("Normalize the features"):
-        for file in files():
-            features, labels, dt = read_single(file)
-            X, y = to_classification(scaler.transform(features), labels, dt)
-            X_res, y_res = downsample(X, y)
-            print("~", X_res.shape, y_res.shape)
-            break
+        X_train, y_train = build_dataset("train", scaler)
 
     with timer("Build the valid set"):
-        X_valid, y_valid = build_valid(
-            "valid",
-            scaler,
-        )
-        print(X_valid.shape, y_valid.shape)
+        X_valid, y_valid = build_dataset("valid", scaler)
 
     with timer("Build the test set"):
-        X_test, y_test = build_valid(
+        # No downsampling for test set to simulate realistic scenario
+        X_test, y_test = build_dataset(
             "test",
             scaler,
-            # No downsampling for test set to simulate realistic scenario
-            downsample=lambda X, y: (X, y),
+            downsample=no_downsample,
         )
-        print(X_test.shape, y_test.shape)
+    print(X_train.shape, y_train.shape)
+    print(X_valid.shape, y_valid.shape)
+    print(X_test.shape, y_test.shape)
 
 
 if __name__ == "__main__":
