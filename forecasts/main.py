c75e7bb4504168574b8bffdd4ea1cc849ac715a3
Sasha <q.dev.null@gmail.com>
2023-06-20 09:49:00 +0000

    Ballance the dataset

--- forecasts/main.py
+++ forecasts/main.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+from imblearn.under_sampling import RandomUnderSampler
 from sklearn.preprocessing import MinMaxScaler
 
 from forecasts.data import files, read_single, to_classification
@@ -15,7 +18,13 @@ def main():
         for file in files():
             features, labels, dt = read_single(file)
             X, y = to_classification(scaler.transform(features), labels, dt)
-            print("~", X.shape, y.shape)
+            shape = X.shape
+            X_res, y_res = RandomUnderSampler().fit_resample(
+                X.reshape(shape[0], -1),
+                y,
+            )
+            print(Counter(y_res))
+            print("~", X_res.reshape(-1, *shape[1:]).shape, y_res.shape)
             continue
     # print(df.head())
 
