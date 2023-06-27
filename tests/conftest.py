83f4e3209f172d90eed17ef12cf5f1ae05da7c74
Sasha <q.dev.null@gmail.com>
2023-06-27 13:33:40 +0200

    Fix the test

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,8 @@ import pathlib
 import numpy as np
 import pytest
 
+from forecasts.fake import fake
+
 
 def legacy_fake(path):
     np.savetxt(path, np.random.rand(149, 10000))
@@ -10,10 +12,19 @@ def legacy_fake(path):
 
 @pytest.fixture
 def fake_dataset(tmp_path: pathlib.Path) -> pathlib.Path:
-    datadir = tmp_path / " data"
+    datadir = tmp_path / " legacy-data"
     datadir.mkdir(exist_ok=True)
     legacy_fake(datadir / "Train_Dst_NoAuction_DecPre_CF_7.txt")
     legacy_fake(datadir / "Test_Dst_NoAuction_DecPre_CF_7.txt")
     legacy_fake(datadir / "Test_Dst_NoAuction_DecPre_CF_8.txt")
     legacy_fake(datadir / "Test_Dst_NoAuction_DecPre_CF_9.txt")
+    return datadir
+
+
+@pytest.fixture
+def dataset(tmp_path: pathlib.Path) -> pathlib.Path:
+    datadir = tmp_path / " data"
+    datadir.mkdir(exist_ok=True)
+    df = fake()
+    df.to_csv(datadir, index=False, header=None)
     return datadir
