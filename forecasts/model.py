e4bf7b9ba8e285dff3ea0b45ff5227e94bc28339
kqf <q.dev.null@gmail.com>
2023-06-28 21:13:44 +0200

    Simplify the training (#27)
    * Restore the original kernel sizes
    
    * Update the training protocol
    
    * Increase the alpha
    
    * Include loss weighting
    
    * Add weighting
    
    * Simplify the data
    
    * Fix the inference

--- forecasts/model.py
+++ forecasts/model.py
@@ -202,11 +202,11 @@ def build_model(
         max_epochs=max_epochs,
         device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
         callbacks=[
-            skorch.callbacks.LRScheduler(
-                policy=torch.optim.lr_scheduler.ReduceLROnPlateau,
-                patience=3,
-                monitor="valid_acc",
-            ),
+            # skorch.callbacks.LRScheduler(
+            #     policy=torch.optim.lr_scheduler.ReduceLROnPlateau,
+            #     patience=3,
+            #     monitor="valid_acc",
+            # ),
             skorch.callbacks.ProgressBar(),
             PlotLossCallback(),
             skorch.callbacks.Checkpoint(
