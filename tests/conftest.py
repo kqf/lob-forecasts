c680b1359962a48532cc06e4bf746566dcb9df5a
Sasha <q.dev.null@gmail.com>
2023-06-30 09:35:32 +0200

    Remove the fake module

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,33 @@
 import pathlib
 
 import numpy as np
+import pandas as pd
 import pytest
 
-from forecasts.fake import fake
+from forecasts.data import COLUMNS, FEATURES
 
 
 def legacy_fake(path):
     np.savetxt(path, np.random.rand(149, 10000))
+
+
+def fake(
+    start="2023-06-27 06:06:00",
+    stop="2023-06-27 10:02:00",
+) -> pd.DataFrame:
+    datetime_range = pd.date_range(start=start, end=stop, freq="30L")
+    # Create a DataFrame with the datetime column
+    df = pd.DataFrame({"Date_time": datetime_range})
+    df["idx"] = df.index
+    df["currency"] = "EURUSD"
+    for c in COLUMNS:
+        if c in df.columns:
+            continue
+        df[c] = np.nan
+
+    for c in FEATURES:
+        df[c] = np.random.rand(*df.index.values.shape)
+    return df
 
 
 @pytest.fixture
