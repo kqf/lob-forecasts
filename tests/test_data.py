59c4f5255c7ef8f72117502debae066918838f4b
kqf <q.dev.null@gmail.com>
2023-07-02 07:10:30 +0200

    Check build data (#35)
    * Uncomment the data
    
    * Uncomment the data
    
    * Train
    
    * Uncomment the learning curve
    
    * Remove the fake module
    
    * Remove the fake data
    
    * Update the tests
    
    * Update the data
    
    * Speed up the checks
    
    * Add the tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,7 @@
-from forecasts.data import files
+from forecasts.data import files, read_single
 
 
 def test_data(dataset):
     filename = next(iter(files("train", directory=dataset)))
-    print(filename)
-    print(dataset)
+    X, y, dt = read_single(filename)
+    print(filename, X.shape, y.shape, dt.shape)
