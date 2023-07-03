38aa4689fc641ab051ad3d40c26d3568b75376b7
Sasha <q.dev.null@gmail.com>
2023-07-03 19:57:43 +0200

    Add the assertions

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,14 @@
 from forecasts.data import files, read_single, to_classification
 
 
-def test_data(dataset):
+def test_data(dataset):  # sourcery skip: extract-duplicate-method
     filename = next(iter(files("train", directory=dataset)))
     features, labels, ticks = read_single(filename)
+    assert features.shape == (1491, 1, 10, 20)
+    assert labels.shape == (1491,)
+    assert ticks.shape == (1491, 1)
+
+    print(features.shape, labels.shape, ticks.shape)
     X, y, dt = to_classification(features, labels, ticks)
     print(filename, X.shape, y.shape, dt.shape)
     assert X.shape == (1491, 1, 10, 20)
