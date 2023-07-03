3a65612c2d75052df970a6421947dcc5001255ad
Sasha <q.dev.null@gmail.com>
2023-07-03 19:54:45 +0200

    Fix namings

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,10 +3,8 @@ from forecasts.data import files, read_single, to_classification
 
 def test_data(dataset):
     filename = next(iter(files("train", directory=dataset)))
-    X, y, dt = read_single(filename)
-    print(filename, X.shape, y.shape, dt.shape)
-    print(dataset)
-    X, y, dt = to_classification(X, y, dt)
+    features, labels, ticks = read_single(filename)
+    X, y, dt = to_classification(features, labels, ticks)
     print(filename, X.shape, y.shape, dt.shape)
     assert X.shape == (1491, 1, 10, 20)
     assert y.shape == (1491,)
