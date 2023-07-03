b873d8b891962c60b624e09c94cfebac34ae63e3
Sasha <q.dev.null@gmail.com>
2023-07-03 19:48:41 +0200

    Fix the data

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,4 +1,4 @@
-from forecasts.data import files, read_single
+from forecasts.data import files, read_single, to_classification
 
 
 def test_data(dataset):
@@ -6,3 +6,8 @@ def test_data(dataset):
     X, y, dt = read_single(filename)
     print(filename, X.shape, y.shape, dt.shape)
     print(dataset)
+    X, y, dt = to_classification(X, y, dt)
+    print(filename, X.shape, y.shape, dt.shape)
+    assert X.shape == (1491, 1, 10, 20)
+    assert y.shape == (1491,)
+    assert dt.shape == (1491, 1)
