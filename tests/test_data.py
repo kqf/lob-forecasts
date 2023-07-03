0d2ac19ff5126f457fcf4a5e551814fe46472ed4
kqf <q.dev.null@gmail.com>
2023-07-03 19:49:57 +0200

    Check classification (#37)
    * Uncomment the data
    
    * Uncomment the data
    
    * Train
    
    * Uncomment the learning curve
    
    * Remove the fake module
    
    * Remove the fake data
    
    * Update the tests
    
    * Update the data
    
    * Speed up the checks
    
    * Add the tests
    
    * Fix the data definition
    
    * Fix the data

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,13 @@
+from forecasts.data import files, read_single, to_classification
 from forecasts.data import files, read_single
 
 
 def test_data(dataset):
     filename = next(iter(files("train", directory=dataset)))
     X, y, dt = read_single(filename)
+    X, y, dt = to_classification(X, y, dt)
     print(filename, X.shape, y.shape, dt.shape)
-    print(dataset)
+    assert X.shape == (1491, 1, 10, 20)
+    assert y.shape == (1491,)
+    assert dt.shape == (1491, 1)
 
