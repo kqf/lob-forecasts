8599f729e4416bf6dec9839ed38c8306a504078e
kqf <q.dev.null@gmail.com>
2023-07-04 19:18:28 +0200

    Test the features shape (#38)
    * Uncomment the data
    
    * Uncomment the data
    
    * Train
    
    * Uncomment the learning curve
    
    * Remove the fake module
    
    * Remove the fake data
    
    * Update the tests
    
    * Update the data
    
    * Speed up the checks
    
    * Add the tests
    
    * Fix the data definition
    
    * Fix the data
    
    * Fix namings
    
    * Add the assertions

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,13 +1,16 @@
 from forecasts.data import files, read_single, to_classification
-from forecasts.data import files, read_single
 
 
-def test_data(dataset):
+def test_data(dataset):  # sourcery skip: extract-duplicate-method
     filename = next(iter(files("train", directory=dataset)))
-    X, y, dt = read_single(filename)
-    X, y, dt = to_classification(X, y, dt)
+    features, labels, ticks = read_single(filename)
+    assert features.shape == (1491, 1, 10, 20)
+    assert labels.shape == (1491,)
+    assert ticks.shape == (1491, 1)
+
+    print(features.shape, labels.shape, ticks.shape)
+    X, y, dt = to_classification(features, labels, ticks)
     print(filename, X.shape, y.shape, dt.shape)
     assert X.shape == (1491, 1, 10, 20)
     assert y.shape == (1491,)
     assert dt.shape == (1491, 1)
-
