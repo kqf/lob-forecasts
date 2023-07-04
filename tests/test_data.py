b40fa70a02bc518d43bdc1c6d1432d97f9bd52c2
Sasha <q.dev.null@gmail.com>
2023-07-04 19:21:36 +0200

    Update the test

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,7 @@
 from forecasts.data import files, read_single, to_classification
 
 
-def test_data(dataset):  # sourcery skip: extract-duplicate-method
+def test_data(dataset):
     filename = next(iter(files("train", directory=dataset)))
     features, labels, ticks = read_single(filename)
     n_obj = len(features)
@@ -12,6 +12,6 @@ def test_data(dataset):  # sourcery skip: extract-duplicate-method
     print(features.shape, labels.shape, ticks.shape)
     X, y, dt = to_classification(features, labels, ticks)
     print(filename, X.shape, y.shape, dt.shape)
-    assert X.shape == (1491, 1, 10, 20)
-    assert y.shape == (1491,)
-    assert dt.shape == (1491, 1)
+    assert X.shape == (len(X), 1, 10, 20)
+    assert y.shape == (len(y),)
+    assert dt.shape == (len(dt), 1)
