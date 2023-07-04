25522315401b8233826c5fffceb56d51870abbfd
Sasha <q.dev.null@gmail.com>
2023-07-04 19:20:29 +0200

    Fix the broken objects

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,9 +4,10 @@ from forecasts.data import files, read_single, to_classification
 def test_data(dataset):  # sourcery skip: extract-duplicate-method
     filename = next(iter(files("train", directory=dataset)))
     features, labels, ticks = read_single(filename)
-    assert features.shape == (1491, 1, 10, 20)
-    assert labels.shape == (1491,)
-    assert ticks.shape == (1491, 1)
+    n_obj = len(features)
+    assert features.shape == (n_obj, 1, 10, 20)
+    assert labels.shape == (n_obj,)
+    assert ticks.shape == (n_obj, 1)
 
     print(features.shape, labels.shape, ticks.shape)
     X, y, dt = to_classification(features, labels, ticks)
